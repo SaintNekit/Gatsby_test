ed931a6b7989334e2c29831b5b9ec3844ed83cbe
SaintNekit <gulivon@mail.ru>
2020-03-30 23:19:38 +0300

    add: deploy to aws s3

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "saintgatsbybucket",
+        protocol: "https",
+        hostname: "https://s3.console.aws.amazon.com"
+      },
+    },
   ],
 }
