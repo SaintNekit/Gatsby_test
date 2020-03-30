fb946798e9be096c2bacbf9797c41959950d9101
SaintNekit <gulivon@mail.ru>
2020-03-30 22:01:22 +0300

    init: init project

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import H1 from '../components/textHeaders'
+import {Link} from 'gatsby'
+
+const HomePage = () => {
+  return (
+  <div style={{ color: "lightblack"}}>
+    <H1 text="This is our home for now" />
+    <H1 text="Not the best one, but the worst also"/>
+    <p>What are we doing here???</p>
+    <Link>Return to main page</Link>
+  </div>
+  )
+}
+
+export default HomePage
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,18 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <div style={{ color: 'black'}}>
+      <h1>Hi morun five</h1>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <br />
+    <Link to="/home">Go to Home page</Link>
   </Layout>
 )
 
